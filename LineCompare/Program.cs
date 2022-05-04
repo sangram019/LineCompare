8d900c989ac6b6c7b6d27d4c881b63352ecc7ac3
Sangram <sangram15.behera@gmail.com>
2022-05-05 00:55:26 +0530

    added by Sangram

---
+++ LineCompare/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch");
+using System;
+namespace LineComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            length test1 = new length();
+            test1.coordinate();
+            equality test2 = new equality();
+            test2.compare();
+        }
+    }
+}
